70537a664e89e564c566a5732756acad35c3fcbe
Aaron Win <aaronjw32@gmail.com>
2020-05-27 06:35:41 +0000

    Revert "v2.1"
    This reverts commit 9da6d074b875e07164cbbfac50690e65a5f9aba2

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -26,18 +26,6 @@ function AppContainer(props) {
                 <Zip onZipChange={handleZipChange} clearResponse={clearResponse}/>
                 <div className="col-sm-4"></div>
             </div>
-            <div id="map"></div>
-            <script>
-              var map;
-              function initMap() {
-                map = new google.maps.Map(document.getElementById('map'), {
-                  center: {lat: -34.397, lng: 150.644},
-                  zoom: 8
-                });
-              }
-            </script>
-            <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-            async defer></script>
             <div className="row mt-4">
                 <div className="col-sm-2"></div>
                 <ZipResponse responseData={responseData} clearResponse={clearResponse}/>
