23e8c24c8b44662647e1a570cff25fe01388f6cc
Aaron Win <aaronjw32@gmail.com>
2020-05-27 01:26:19 +0000

    5

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -38,7 +38,7 @@ describe('Get Weather', function() {
   it('with valid zip code and error from request call', function() {
     reqMock = {
       query: {
-        zip: 'El Paso'
+        zip: 'Hamilton'
       }
     };
 
@@ -57,7 +57,7 @@ describe('Get Weather', function() {
   it('with incomplete zip code', function() {
     reqMock = {
       query: {
-        zip: 'El Paso'
+        zip: 'Hamilton'
       }
     };
 
@@ -76,13 +76,13 @@ describe('Get Weather', function() {
   it('with valid zip code', function() {
     reqMock = {
       query: {
-        zip: 'El Paso'
+        zip: 'Hamilton'
       }
     };
 
     const body = {
       cod: 200,
-      name: 'El Paso',
+      name: 'Hamilton',
       weather: [
         {
           main: 'cold'
